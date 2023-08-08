17128e5a78aea8ee4a65c5004d9cf6b9aea86d28
christian burns <christianburns03@gmail.com>
2023-08-08 17:15:26 -0300

    final

--- src/componentes/TareaFormulario.js
+++ src/componentes/TareaFormulario.js
@@ -6,36 +6,40 @@ import { v4 as uuidv4 } from 'uuid';
 function TareaFormulario(props) {
 
     const [input, setInput] = useState('');
-
+  
     const manejarCambio = e => {
-        setInput(e.target.value);        
+      setInput(e.target.value);
     }
-
+  
     const manejarEnvio = e => {
-        e.preventDefault();
-
-
-        const tareaNueva = {
-            id: uuidv4(),
-            Texto: input,
-            completada: false
-        }
-
-        props.onSubmit(tareaNueva);        
+      e.preventDefault();
+      
+      const tareaNueva = {
+        id: uuidv4(),
+        texto: input,
+        completada: false
+      }
+  
+      props.onSubmit(tareaNueva);
     }
 
 
     return (
-        <form className="tarea-formulacio" onSubmit={manejarEnvio}>
-            <input className="tarea-input"
-            type="text"
-            placeholder="Escribe una Tarea"
-            name="texto" 
-            onChange={manejarCambio}            
-            />
-            <button className="tarea-boton">Agregar Tarea</button>
+        <form 
+          className='tarea-formulario'
+          onSubmit={manejarEnvio}>
+          <input 
+            className='tarea-input'
+            type='text'
+            placeholder='Escribe una Tarea'
+            name='texto'
+            onChange={manejarCambio}
+          />
+          <button className='tarea-boton'>
+            Agregar Tarea
+          </button>
         </form>
-    );
-}
-
-export default TareaFormulario;
+      );
+    }
+    
+    export default TareaFormulario;
